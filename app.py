c1b345c6a82fbef9231e4e6212830ccb27fbdcca
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2025-02-20 12:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, jsonify, render_template
+import ollama  
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('./index.html')
+    return render_template('index.html')
+
+def call_model(prompt):
+    try:
+        response = ollama.chat(model="orca-mini", messages=prompt)
+        return response.get("text", "No response received from the model.")
+    except Exception as e:
+        print("Error")
+        return e
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    user_input = request.json.get("user_input")  
+    user_input = request.json.get("user_input")
     print(f"User input: {user_input}")
-    bot_response = f"You said: {user_input}"
-
+    prompt = f"Generate a detailed travel itinerary based on this input : {user_input}."
+    bot_response = call_model(prompt)
     return jsonify({'bot_response': bot_response})
 
 if __name__ == "__main__":
