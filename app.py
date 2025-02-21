8c523e01ddf8e507e8c4d6ae4ad941841361886f
abhishekcarmel01 <abhishekcarmel01@gmail.com>
2025-02-21 17:21:23 +0530

    model done

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def home():
 
 def call_model(prompt):
     try:
-        response = ollama.chat(model="orca-mini", messages=prompt)
-        return response.get("text", "No response received from the model.")
+        response = ollama.chat(model="orca-mini", messages=[{"role": "user", "content": prompt}])
+        return response.get("message", {}).get("content", "No response received from the model.")
     except Exception as e:
         print("Error")
         return e
@@ -18,8 +18,8 @@ def call_model(prompt):
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.json.get("user_input")
-    print(f"User input: {user_input}")
-    prompt = f"Generate a detailed travel itinerary based on this input : {user_input}."
+    #print(f"User input: {user_input}")
+    prompt = f"Generate a detailed travel itinerary based on this location : {user_input}."
     bot_response = call_model(prompt)
     return jsonify({'bot_response': bot_response})
 
