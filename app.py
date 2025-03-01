a2120f366e6c25707b236e2f4c583a067ade8bc9
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2025-03-01 10:14:17 +0530

    do tests

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 import ollama  
 from chroma import travel_embeddings, query_input
 import json
 from dotenv import load_dotenv
 import os
-from flask_session import Session
 import spacy 
 
 nlp = spacy.load("en_core_web_sm")
@@ -13,14 +12,14 @@ load_dotenv()
 SECRET_KEY=os.getenv("FLASK_SECRET_KEY")
 
 app = Flask(__name__)
-app.config['SECRET_KEY']=SECRET_KEY
-app.config['sESSION_TYPE']='filesystem'
-Session(app)
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SESSION_TYPE'] = 'filesystem'
+session(app)
 
 @app.route('/')
 def home():
-    Session['history']=[]
-    Session['travel_data']=[]
+    session['history']=[]
+    session['travel_data']=[]
     return render_template('index.html')
 
 def initial_prompt(res):
@@ -31,7 +30,7 @@ def initial_prompt(res):
 def extract(user_input):
     doc=nlp(user_input)
     dest=[ent.text for ent in doc.ents if ent.label_=="GPE"]
-    return dest[0]
+    return {'destination':dest[0]} if dest else {'destination':"Unknown"}
 
 def build_prompt(conv_history,user_prompt):
     history="\n".join(conv_history)
@@ -49,23 +48,24 @@ def call_model(prompt):
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.json.get("user_input")
-    conv_history = Session.get('history', [])
-    travel_data = Session.get('travel_data', [])
-    if not travel_data or (extract(user_input).lower()!=travel_data['destination'].lower()):
+    conv_history = session.get('history', [])
+    travel_data = session.get('travel_data', [])
+
+    if not travel_data or (extract(user_input)['destination'].lower()!=travel_data['destination'].lower()):
         travel_data = extract(user_input)
-        Session['travel_data']=travel_data
+        session['travel_data']=travel_data
 
     collec=travel_embeddings(travel_data['destination'])
     res=query_input(collec, user_input)
-    if not Session.get('system_prompt'):
+    if not session.get('system_prompt'):
         sys_prompt=initial_prompt(res)
         conv_history.append(f"System: {sys_prompt}")
-        Session['system_prompt']=sys_prompt
+        session['system_prompt']=sys_prompt
 
     full_prompt=build_prompt(conv_history, user_input)
     bot_response=call_model(full_prompt)
     conv_history.append(f"User: {user_input}\nBot: {bot_response}")
-    Session['history']=conv_history
+    session['history']=conv_history
     return jsonify({'bot_response': bot_response})
 
 if __name__ == "__main__":
