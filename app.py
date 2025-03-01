83ff6bc61bf8e85f557db19f642a9747d24c601d
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2025-03-01 09:50:37 +0530

    do tests

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ load_dotenv()
 SECRET_KEY=os.getenv("FLASK_SECRET_KEY")
 
 app = Flask(__name__)
-app.config('SECRET_KEY')=SECRET_KEY
-app.config('sESSION_TYPE')='filesystem'
+app.config['SECRET_KEY']=SECRET_KEY
+app.config['sESSION_TYPE']='filesystem'
 Session(app)
 
 @app.route('/')
@@ -27,13 +27,15 @@ def initial_prompt(res):
     return( "You are a travel assistant that will build travel iternaries based on user's needs"
            f"Include the following attractions: {res}."
            )
+
 def extract(user_input):
     doc=nlp(user_input)
     dest=[ent.text for ent in doc.ents if ent.label_=="GPE"]
     return dest[0]
+
 def build_prompt(conv_history,user_prompt):
     history="\n".join(conv_history)
-    prompt=f"{history}\nUser: {user_prompt}\nBot:"
+    prompt=f"{history}\nUser: {user_prompt}"
     return prompt
 
 def call_model(prompt):
@@ -49,13 +51,17 @@ def chat():
     user_input = request.json.get("user_input")
     conv_history = Session.get('history', [])
     travel_data = Session.get('travel_data', [])
-    if not travel_data or user_input.lower().find(travel_data['destination'].lower()) == -1:
+    if not travel_data or (extract(user_input).lower()!=travel_data['destination'].lower()):
         travel_data = extract(user_input)
         Session['travel_data']=travel_data
-        collec=travel_embeddings(travel_data['destination'])
-        res=query_input(collec, user_input)
-        conv_history.append(f"System: {initial_prompt(res)}")
-    #print(f"User input: {user_input}")
+
+    collec=travel_embeddings(travel_data['destination'])
+    res=query_input(collec, user_input)
+    if not Session.get('system_prompt'):
+        sys_prompt=initial_prompt(res)
+        conv_history.append(f"System: {sys_prompt}")
+        Session['system_prompt']=sys_prompt
+
     full_prompt=build_prompt(conv_history, user_input)
     bot_response=call_model(full_prompt)
     conv_history.append(f"User: {user_input}\nBot: {bot_response}")
